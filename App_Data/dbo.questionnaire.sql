d170420d218ce4b1aed5cc612cb9393a423bd040
hibat allah labhiri <hibatallahlabhiri@gmail.com>
2022-09-21 18:56:14 +0100

    first commit

---
+++ App_Data/dbo.questionnaire.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[questionnaire] (
+    [idQuest]    INT           NOT NULL,
+    [titreQuest] VARCHAR (100) NULL,
+    [dateouverture]  DATE          NULL,
+	[datefermeture]  DATE          NULL,
+    [etatQuest]  VARCHAR (50)  NULL,
+    [urlQuest]   VARCHAR (100) NULL,
+    CONSTRAINT [PK_questionnaire] PRIMARY KEY CLUSTERED ([idQuest] ASC)
+);
+
